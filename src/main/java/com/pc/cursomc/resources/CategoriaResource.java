0cf06bcfc0342d63f3badd5f7db7cdad1035dd6c
Paulo Cesar <paulocesar.araujo.ads@gmail.com>
2020-08-25 21:52:44 -0300

    Criando categoriaService e Repository

--- src/main/java/com/pc/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/pc/cursomc/resources/CategoriaResource.java
@@ -3,26 +3,35 @@ package com.pc.cursomc.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pc.cursomc.domain.Categoria;
+import com.pc.cursomc.services.CategoriaService;
 
 @RestController
-@RequestMapping(value = "categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	@GetMapping
-	public List<Categoria> listar() {
+	
+	@Autowired
+	private CategoriaService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Categoria> listar(@PathVariable Integer id) {
+		Categoria obj = service.find(id);
 		
-		Categoria cat1 = new Categoria(1, "Escritorio");
-		Categoria cat2 = new Categoria(2, "Informatica");
+//		Categoria cat1 = new Categoria(1, "Escritorio");
+//		Categoria cat2 = new Categoria(2, "Informatica");
+//		
+//		List<Categoria> lista = new ArrayList<>();
+//		lista.add(cat1);
+//		lista.add(cat2);
 		
-		List<Categoria> lista = new ArrayList<>();
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/com/pc/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.pc.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pc.cursomc.domain.Categoria;
+import com.pc.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		 Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		} 
+
+
+}
